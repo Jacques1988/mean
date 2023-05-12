27f3b84d742486ab4210dbf5bd441fd3a43e707a
Jacques <jacquesvanluyck@gmail.com>
2023-05-12 20:18:19 +0200

    update http request

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,26 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use((request, result, next) => {
   result.setHeader("Access-Control-Allow-Origin", "*");
-  result.setHeader("Access-Control-Allow-Header", "Origin, X-Request-With, Content-Type, Accept");
+  result.setHeader("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept");
   result.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
 
 
-app.use('/api/posts', (request, response, next) => {
+app.post("/api/posts", (request, response, next) => {
+  const posts = request.body;
+  console.log(posts);
+  response.status(201).json({message: 'Post added successfully'});
+});
+
+app.get('/api/posts', (request, response, next) => {
   const posts = [
     {
       id: '123456',
