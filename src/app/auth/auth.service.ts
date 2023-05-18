e1e8a9a1d4f96623fd3b69535d87adaa086a4b8f
Jacques <jacquesvanluyck@gmail.com>
2023-05-18 13:07:59 +0200

    update spinner on isLoading

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { AuthData } from './auth-data.model';
 import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 
 
 
@@ -11,21 +12,22 @@ export class AuthService {
   private isAuthenticated = false;
   private token;
   private authStatusListener = new Subject<boolean>();
-  constructor(private http: HttpClient) { }
+
+
+  constructor(private http: HttpClient, private router : Router) { }
 
 
   getToken() {
     return this.token;
   }
 
-  getIsAuth(){
+  getIsAuth() {
     return this.isAuthenticated;
   }
 
   getAuthStatusListenener() {
     return this.authStatusListener.asObservable();
   }
-
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -43,8 +45,17 @@ export class AuthService {
       if (token) {
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
+        this.router.navigate(['/']);
       }
 
     })
+  }
+
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,7 +22,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
     });
   }
 
-
+onLogout(){
+  this.authService.logout();
+}
 
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
