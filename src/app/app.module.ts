ee241263d79e2dc8308cc51b5ce21d71f35f34e2
Jacques <jacquesvanluyck@gmail.com>
2023-05-20 13:25:09 +0200

    update errorhandling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { ErrorComponent } from './error/error.component';
 
 import { MatInputModule} from '@angular/material/input';
 import { MatCardModule} from '@angular/material/card';
@@ -19,8 +20,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+
 
 
 @NgModule({
@@ -30,7 +34,8 @@ import { ErrorInterceptor } from './error-interceptor';
     HeaderComponent,
     PostListComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ErrorComponent
 
   ],
   imports: [
@@ -46,6 +51,7 @@ import { ErrorInterceptor } from './error-interceptor';
     MatExpansionModule,
     MatProgressSpinnerModule,
     MatPaginatorModule,
+    MatDialogModule,
     HttpClientModule
   ],
   providers: [
@@ -53,6 +59,7 @@ import { ErrorInterceptor } from './error-interceptor';
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
   ],
 
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent]
 })
 export class AppModule { }
--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpHandler, HttpRequest, HttpInterceptor, HttpErrorResponse } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
+import { Injectable } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog";
+import { ErrorComponent } from "./error/error.component";
 
 
-
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
 
 
@@ -13,8 +16,11 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.error.message);
+        let errorMessage = "An unknown error occured!";
+        if(error.error.message){
+          errorMessage = error.error.message
+        }
+        this.dialog.open(ErrorComponent, {data: {message: errorMessage }});
        return throwError(error)
       })
     )
